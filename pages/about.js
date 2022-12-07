6660518e27fc75e7c042e951355edd3722e77828
Kumar Pandule <kumarpandule20@gmail.com>
2022-12-07 22:47:01 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -23,24 +23,24 @@ export default function About() {
       </header>
       <h2 className="text-center md:text-left">Our Mission</h2>
       <p>
-        Health monitoring system is pune’s growing health monitoring site to let
-        the hospital manage their patient's health paramaters anywhere, anytime.
-        Our aim is to make patient’s health parameter accessible to doctors all
+        Health monitoring system is {"pune’s"} growing health monitoring site to let
+        the hospital manage their {"patient's"} health paramaters anywhere, anytime.
+        Our aim is to make {"patient’s"} health parameter accessible to doctors all
         over the world for the quick treatment. Our product is designed with
         latest tech in market to enhance the performance ahead and make
         organizations become advance at treatment in no time.
       </p>
-      <h2 className="text-center md:text-left">what's in it for you</h2>
+      <h2 className="text-center md:text-left">{"what's"} in it for you</h2>
       <p>
-        The Health monitoring system's Kit will let the doctor or the nurse
-        measure the patient's Body temperature, heart rate which will
+        The Health monitoring {"system's"} Kit will let the doctor or the nurse
+        measure the {"patient's"} Body temperature, heart rate which will
         automatically update the real-time data from the kit to WeCare Website
         which will be accessible to the organization as well as doctor remotely.
       </p>
       <h2 className="text-center md:text-left">what we do</h2>
       <p>
-        We let the organization monitor their patient's health parameter by
-        collecting the data from the WeCare kit provided to the organization.{" "}
+        We let the organization monitor their {"patient's"} health parameter by
+        collecting the data from the WeCare kit provided to the organization.
       </p>
       <h2 className="text-center md:text-left">Terms &amp;&amp; Conditions</h2>
       <p>
